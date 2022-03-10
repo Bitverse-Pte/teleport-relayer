1c2c0bfe822b6c9ea894f3f188326c2be9408d8c
curryxbo <curryx1992@gmail.com>
2022-03-10 16:58:07 +0800

    fix RevisedHeight

--- app/jobs/channels/channel_manager.go
+++ app/jobs/channels/channel_manager.go
@@ -43,14 +43,14 @@ func NewChannelMap(cfg *config.Config, logger *logrus.Logger) map[string]IChanne
 func MakeChannels(cfg *config.Config, sourceChain, destChain interfaces.IChain, logger *logrus.Logger) map[string]IChannel {
 	channelMap := make(map[string]IChannel)
 	if cfg.Chain.Source.Enable {
-		srcChannel, err := NewChannel(sourceChain, destChain, cfg.Chain.Source.Cache.StartHeight, cfg.Chain.Source.Cache.Filename,cfg.Chain.Source.RelayFrequency, logger)
+		srcChannel, err := NewChannel(sourceChain, destChain, cfg.Chain.Source.Cache.StartHeight,cfg.Chain.Source.Cache.RevisedHeight, cfg.Chain.Source.Cache.Filename,cfg.Chain.Source.RelayFrequency, logger)
 		if err != nil {
 			panic(fmt.Errorf("srcchannel create error:%+v",err))
 		}
 		channelMap[sourceChain.ChainName()] = srcChannel
 	}
 	if cfg.Chain.Dest.Enable {
-		destChannel, err := NewChannel(destChain, sourceChain, cfg.Chain.Dest.Cache.StartHeight, cfg.Chain.Dest.Cache.Filename,cfg.Chain.Dest.RelayFrequency, logger)
+		destChannel, err := NewChannel(destChain, sourceChain, cfg.Chain.Dest.Cache.StartHeight,cfg.Chain.Dest.Cache.RevisedHeight, cfg.Chain.Dest.Cache.Filename,cfg.Chain.Dest.RelayFrequency, logger)
 		if err != nil {
 			panic(fmt.Errorf("destChannel create error:%+v",err))
 		}
